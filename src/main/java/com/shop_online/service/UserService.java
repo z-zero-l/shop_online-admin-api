9cd7ba5a953b4db34c49fc5e88a72c2d86e82e24
zero <z.zero.l@outlook.com>
2023-11-28 15:03:50 +0800

    feat: 用户模块-获取用户记录、导出用户记录

--- src/main/java/com/shop_online/service/UserService.java
+++ src/main/java/com/shop_online/service/UserService.java
@@ -1,16 +1,37 @@
 package com.shop_online.service;
 
+import com.shop_online.common.model.Query;
+import com.shop_online.common.result.PageResult;
 import com.shop_online.entity.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.shop_online.vo.UserVO;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author zero
  * @since 2023-11-28
  */
 public interface UserService extends IService<User> {
+
+    /**
+     * 获取用户记录
+     *
+     * @param query
+     * @return PageResult
+     */
+    PageResult<UserVO> getPage(Query query);
+
+    /**
+     * 导出用户记录
+     *
+     * @param query
+     * @param response
+     */
+    void exportUserInfo(Query query, HttpServletResponse response);
 
 }
